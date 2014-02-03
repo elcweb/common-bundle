955f6db06f8fc5e993c9dc4062f8efc6f5a521b4
Etienne Lachance <et@etiennelachance.com>
2014-02-03 10:06:27 -0500

    Mink getSymfonyProfile

--- Features/Context/SymfonyMinkContext.php
+++ Features/Context/SymfonyMinkContext.php
@@ -110,4 +110,49 @@ abstract class SymfonyMinkContext extends MinkContext implements KernelAwareInte
 
         return $profile;
     }
+
+    /**
+     * @Given /^I should get an email on "([^"]*)" with "([^"]*)"$/
+     */
+    public function iShouldGetAnEmail($email, $text)
+    {
+        $error     = sprintf('No message sent to "%s"', $email);
+        $profile   = $this->getSymfonyProfile();
+        $collector = $profile->getCollector('swiftmailer');
+
+        foreach ($collector->getMessages() as $message) {
+            // Checking the recipient email and the X-Swift-To
+            // header to handle the RedirectingPlugin.
+            // If the recipient is not the expected one, check
+            // the next mail.
+            $correctRecipient = array_key_exists(
+                $email, $message->getTo()
+            );
+            $headers = $message->getHeaders();
+            $correctXToHeader = false;
+            if ($headers->has('X-Swift-To')) {
+                $correctXToHeader = array_key_exists($email,
+                    $headers->get('X-Swift-To')->getFieldBodyModel()
+                );
+            }
+
+            if (!$correctRecipient && !$correctXToHeader) {
+                continue;
+            }
+
+            try {
+                // checking the content
+                return assertContains(
+                    $text->getRaw(), $message->getBody()
+                );
+            } catch (AssertException $e) {
+                $error = sprintf(
+                    'An email has been found for "%s" but without '.
+                    'the text "%s".', $email, $text->getRaw()
+                );
+            }
+        }
+
+        throw new ExpectationException($error, $this->getSession());
+    }
 }
